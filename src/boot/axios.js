66f469bc0cd40461c4af30f59d1ac76d9f8c4022
Algirdas <algirdas@zyro.com>
2020-10-11 22:40:46 +0300

    Log API_URL

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,18 +1,20 @@
-import Vue from 'vue'
-import axios from 'axios'
+import Vue from "vue";
+import axios from "axios";
 
-export const LOG_IN = 'LOG_IN';
-export const LOG_OUT = 'LOG_OUT';
-export const CHECK_LOCALSTORAGE = 'CHECK_LOCALSTORAGE';
+export const LOG_IN = "LOG_IN";
+export const LOG_OUT = "LOG_OUT";
+export const CHECK_LOCALSTORAGE = "CHECK_LOCALSTORAGE";
 
-Vue.prototype.$axios = axios
-axios.interceptors.request.use((config) => ({
-    ...config,
-    baseURL: process.env.API_URL,
-    headers: {
-        ...config.headers,
-        common: {
-            Accept: 'application/json',
-        }
-    },
-}))
+Vue.prototype.$axios = axios;
+
+console.log(process.env.API_URL);
+axios.interceptors.request.use(config => ({
+  ...config,
+  baseURL: process.env.API_URL,
+  headers: {
+    ...config.headers,
+    common: {
+      Accept: "application/json"
+    }
+  }
+}));
